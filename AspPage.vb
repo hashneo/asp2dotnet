487f74e60e44604155cec48436a2192a424c848c
Steven Taylor <steven.taylor@me.com>
2015-10-01 15:06:04 -0700

    fix so we can use asppage is global.asax

--- AspPage.vb
+++ AspPage.vb
@@ -1,6 +1,35 @@
 Public Class AspPage
 	Inherits Page
 
+	Private _HttpApplication As System.Web.HttpApplication
+
+	Public Overrides ReadOnly Property Session As HttpSessionState
+		Get
+			If (Not _HttpApplication Is Nothing) Then
+				Return _HttpApplication.Session
+			End If
+			Return MyBase.Session
+		End Get
+	End Property
+
+	Public ReadOnly Property Application As System.Web.HttpApplicationState
+		Get
+			If (Not _HttpApplication Is Nothing) Then
+				Return _HttpApplication.Application
+			End If
+			Return MyBase.Application
+		End Get
+	End Property
+
+	Public ReadOnly Property Server As System.Web.HttpServerUtility
+		Get
+			If (Not _HttpApplication Is Nothing) Then
+				Return _HttpApplication.Server
+			End If
+			Return MyBase.Server
+		End Get
+	End Property
+
 	Private classCache As Dictionary(Of String, PageClass) = New Dictionary(Of String, PageClass)
 
 	Public Function createInstance(What As System.Type) As PageClass
@@ -15,6 +44,16 @@ Public Class AspPage
 	End Function
 
 	Function Response_WriteLine(Str As String)
-		Response.Write(Str & vbCrLf)
+		If Not Response Is Nothing Then
+			Response.Write(Str & vbCrLf)
+		End If
 	End Function
+
+	Public Sub New()
+		MyBase.New()
+	End Sub
+
+	Public Sub New(HttpApplication As System.Web.HttpApplication)
+		Me._HttpApplication = HttpApplication
+	End Sub
 End Class
